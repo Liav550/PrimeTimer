f8ea1d510b152de0a104f9d120b7ca7b3fe9884b
liavb <tchstud2025l2604@gmail.com>
2025-12-30 14:56:02 +0200

    feat(several): add login page that works responsively

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 
+document.body.style.margin = "0";
+document.body.style.padding = "0";
+document.body.style.boxSizing = "border-box";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const colorPallete = {
+  primary: "#F5DAA7",
+  secondary: "#fbce7aff",
+  accent: "#e0bb78ff",
+  bold: "#682e2eff",
+};
